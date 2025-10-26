444dab20a68ce5ea95a9c932d6a2f3a00200f863
Akinseloyin Elijah Oluwademilade <elijah@localhost.localdomain>
2025-10-26 19:50:09 +0100

    Configure Strapi content types and project setup

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,33 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:1337', // Strapi itself
+
+        // Your test/dev servers
+        'http://localhost:5500', 
+        'http://127.0.0.1:5500', 
+
+        // Your new Apache server
+        'http://localhost:8080', 
+        'http://127.0.0.1:8080',
+
+        // Your future Cloudflare domain
+        'https://monarchdem.me' // CHANGE THIS
+      ],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  // ... rest of the file
 ];
