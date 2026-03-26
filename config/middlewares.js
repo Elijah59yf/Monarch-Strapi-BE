e04f3a9e458ec74460c1ccbf35a339f37001781e
Elijah59yf <91700703+Elijah59yf@users.noreply.github.com>
2026-03-26 20:06:12 +0100

    Add new domain to middlewares configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,6 +15,7 @@ module.exports = [
         // Your new Apache server
         'http://localhost:8080', 
         'http://127.0.0.1:8080',
+        'https://console.monarchdem.me',
 
         // Your future Cloudflare domain
         'https://monarchdem.me', // CHANGE THIS
